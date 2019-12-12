efd37dd537e3760f1a7d2912fd19386fc53b4f6c
PaBLyS <pavlobokhan@gmail.com>
2019-12-12 12:00:22 +0200

    create defaultText component

---
+++ components/defaultText.vue
@@ -0,0 +1,24 @@
+<template>
+    <b-row class="text-wrap">
+      <b-col cols="12">
+        {{defaultText}}
+      </b-col>
+    </b-row>
+</template>
+
+<script>
+    export default {
+        name: "defaultText",
+        props: {
+            defaultText: String
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+  .text-wrap {
+    width: 100%;
+    background: #dcdcdc;
+    padding: 30px;
+  }
+</style>
