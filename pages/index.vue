9cf05c2030c8943902058edff7791435de0ca4ee
PaBLyS <pavlobokhan@gmail.com>
2019-12-13 16:44:02 +0200

    create login page

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,25 @@
 <template>
-  <section>
-    <form action="">
-      <input v-model="login" type="text">
-      <input v-model="password" type="text">
-      <button @click="signIn">Вход</button>
+  <section class="index">
+    <form class="index__form">
+      <span class="index__form-label">Login</span>
+      <input v-model="login"
+             type="text"
+             class="index__form-input"
+             placeholder="Login">
+      <input v-model="password"
+             type="text"
+             class="index__form-input"
+             placeholder="Password">
+      <div class="index__form-button"
+           @click="signIn">
+        Login
+      </div>
     </form>
   </section>
 </template>
 
 <script>
-
     export default {
-        components: {},
         data() {
             return {
                 login: '',
@@ -30,5 +38,63 @@
 </script>
 
 <style lang="scss">
+  .index {
+    min-height: 100vh;
+    min-width: 100%;
+    background: linear-gradient(125.42deg, #FFFFFF 0%, #DADADA 100%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
+    &__form {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      width: 555px;
+      height: 547px;
+      background: #F4F4F4;
+      box-shadow: 30px 30px 80px rgba(0, 0, 0, 0.5), -30px -30px 80px #FFFFFF, inset 4px 4px 20px rgba(255, 255, 255, 0.25);
+      border-radius: 30px;
+      padding: 65px 30px;
+
+      &-label {
+        font-family: 'Roboto', sans-serif;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 36px;
+        line-height: 42px;
+        display: flex;
+        color: #4F4F4F;
+        align-self: flex-start;
+      }
+
+      &-input {
+        border: 1px solid #BDBDBD;
+        border-radius: 10px;
+        background: transparent;
+        padding: 13px 10px;
+        font-family: 'Roboto', sans-serif;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 16px;
+        outline: none;
+      }
+
+      &-button {
+        display: inline-block;
+        padding: 14px 51px;
+        align-self: flex-end;
+        background: #27AE60;
+        border-radius: 10px;
+        font-family: 'Roboto', sans-serif;
+        font-style: normal;
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 16px;
+        color: #F2F2F2;
+        cursor: pointer;
+      }
+    }
+  }
 </style>
