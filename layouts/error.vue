ea61f4c75d2c93c6c0ff0cecf6dab55e0d279829
PaBLyS <pavlobokhan@gmail.com>
2019-12-16 15:59:18 +0200

    create error page

---
+++ layouts/error.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="error">
+    <div class="error__wrap">
+      <p class="error__code">Error {{error.statusCode}}</p>
+      <p class="error__message">{{error.message}}</p>
+      <p>
+        <nuxt-link to="/">
+          Home
+        </nuxt-link>
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+    export default {
+        props: ['error'],
+        name: "error"
+    }
+</script>
+
+<style lang="scss" scoped>
+  .error {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: linear-gradient(125.42deg, #FFFFFF 0%, #DADADA 100%);
+
+    &__wrap {
+      text-align: center;
+    }
+
+    &__code {
+      color: red;
+      font-size: 48px;
+      font-weight: bold;
+    }
+
+    &__message {
+      font-size: 28px;
+    }
+  }
+</style>
