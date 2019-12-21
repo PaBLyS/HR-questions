b93a051ffb324e4a762e1ed127d9dac33b9566f7
PaBLyS <pavlobokhan@gmail.com>
2019-12-21 17:58:23 +0200

    create admin page

---
+++ pages/admin/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="admin">
+    <b-container>
+      <b-row>
+        <b-col cols="12">
+          <div class="admin__row">
+            Questions
+          </div>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="12">
+          <div class="admin__row">
+            Vacancies
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </section>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style lang="scss" scoped>
+  .admin {
+    min-height: 100vh;
+    background: linear-gradient(125.42deg, #FFFFFF 0%, #DADADA 100%);
+    padding: {
+      top: 115px;
+    }
+
+    &__row {
+      padding: 19px 0;
+      margin-bottom: 30px;
+      font-family: 'Roboto', sans-serif;
+      font-style: normal;
+      font-weight: 500;
+      font-size: 36px;
+      line-height: 42px;
+      display: flex;
+      align-items: center;
+      text-indent: 20px;
+      color: #4F4F4F;
+      background: #F4F4F4;
+      box-shadow: 7px 7px 40px rgba(0, 0, 0, 0.2), -7px -7px 40px rgba(255, 255, 255, 0.35), inset 4px 4px 20px rgba(255, 255, 255, 0.25);
+      border-radius: 10px;
+      cursor: pointer;
+    }
+  }
+</style>
---
+++ pages/admin/questions.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="edit-questions">
+    <b-container>
+      <b-row>
+        <b-col>
+          <div>
+            <div>
+              <input type="text">
+              <b-button-group>
+                <b-button variant="success">Add</b-button>
+              </b-button-group>
+            </div>
+            <div>
+              <textarea></textarea>
+            </div>
+          </div>
+        </b-col>
+        <b-col cols="12" >
+          <div v-for="(elem, index) in editQuestion" :key="index">
+            <div>
+              <input type="text" :value="elem.label">
+              <b-button-group>
+                <b-button variant="success">Save</b-button>
+                <b-button variant="danger">Delete</b-button>
+              </b-button-group>
+            </div>
+            <div>
+              <textarea></textarea>
+            </div>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </section>
+</template>
+
+<script>
+    export default {
+        name: "questions",
+        beforeCreate() {
+            this.$store.dispatch('fetchQuestions');
+        },
+        beforeMount() {
+            this.editQuestion = this.$store.getters.questions;
+        },
+        data() {
+          return {
+              editQuestion: []
+          }
+        },
+        computed: {
+
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+  .edit-questions {
+    min-height: 100vh;
+    background: linear-gradient(125.42deg, #FFFFFF 0%, #DADADA 100%);
+    padding: {
+      top: 115px;
+    }
+  }
+</style>
